1ed5bf8730a8dbc712b881cca9b1b55c179b5a5e
Prerna <myemail.com>
2024-08-28 16:21:37 +0530

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://github.com/prerna-gurung/Keeper-notes-app', 
+  base: 'https://github.com/prerna-gurung/Keeper-notes-app/', 
   plugins: [react()],
 })
